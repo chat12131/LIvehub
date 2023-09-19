f5d4732f618664e546909b7b7b4c694be6b2b933
sasaki <chat1027201@gmail.com>
2023-09-20 03:39:15 +0900

    ユーザー情報編集ページのrspec

--- spec/requests/users/user_edit_spec.rb
+++ spec/requests/users/user_edit_spec.rb
@@ -1,38 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe 'ユーザー編集', type: :request do
+RSpec.describe 'ユーザー編集' do
   describe 'PUT /users' do
     let(:user) { create(:user) }
-    let(:新しいユーザーネーム) { '新しいユーザーネーム' }
-    let(:新しいメールアドレス) { Faker::Internet.email }
+    let(:new_username) { 'ユーザー' }
+    let(:new_email) { Faker::Internet.email }
 
     before do
       sign_in user
       get edit_user_registration_path
     end
 
-    context 'ユーザーネームだけを更新する場合' do
-      it 'ユーザーネームが更新される' do
-        put user_registration_path, params: { user: { username: 新しいユーザーネーム } }
-        expect(response).to redirect_to(root_path)
-        expect(user.reload.username).to eq(新しいユーザーネーム)
-      end
+    it '現在のパスワードなしではユーザーネームとメールアドレスが更新されない' do
+      put user_registration_path, params: { user: { username: new_username, email: new_email } }
+      expect(user.reload.username).not_to eq(new_username)
+      expect(user.reload.email).not_to eq(new_email)
     end
 
-    context 'ユーザーネームとメールアドレスを更新する場合' do
-      it '現在のパスワードなしではユーザーネームとメールアドレスが更新されない' do
-        put user_registration_path, params: { user: { username: 新しいユーザーネーム, email: 新しいメールアドレス } }
-        expect(response).to redirect_to(edit_user_registration_path)
-        expect(user.reload.username).not_to eq(新しいユーザーネーム)
-        expect(user.reload.email).not_to eq(新しいメールアドレス)
-      end
-
-      it '現在のパスワードありでユーザーネームとメールアドレスが更新される' do
-        put user_registration_path, params: { user: { username: 新しいユーザーネーム, email: 新しいメールアドレス, current_password: user.password } }
-        expect(response).to redirect_to(root_path)
-        expect(user.reload.username).to eq(新しいユーザーネーム)
-        expect(user.reload.email).to eq(新しいメールアドレス)
-      end
+    it '現在のパスワードありでユーザーネームとメールアドレスが更新される' do
+      put user_registration_path, params: { user: { username: new_username, email: new_email, current_password: user.password } }
+      expect(response).to redirect_to(root_path)
+      expect(user.reload.username).to eq(new_username)
+      expect(user.reload.email).to eq(new_email)
     end
   end
 end
--- spec/system/users/user_edit_spec.rb
+++ spec/system/users/user_edit_spec.rb
@@ -1,41 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe "UserEdits", type: :system do
+RSpec.describe "UserEdits" do
   describe "username update" do
-  let(:user) { create(:user) }
+    let(:user) { create(:user) }
+    let(:new_email) { Faker::Internet.email }
 
-  before do
-    sign_in user
-    visit edit_user_registration_path
-    new_email = Faker::Internet.email
-  end
-
-  context "usernameのみ変更する場合" do
-    it "usernameが変更できること" do
-      fill_in "ユーザーネーム", with: "new_username"
-      click_button "変更"
-      expect(current_path).to eq(root_path)
-      expect(user.reload.username).to eq(new_username)
-    end
-  end
-
-  context "username以外も変更する場合" do
-    it "usernameとemailを変更できないこと" do
-      fill_in "ユーザーネーム", with: "new_username"
-      fill_in "メールアドレス", with: Faker::Internet.email
-      click_button "変更"
-      expect(current_path).to eq(edit_user_registration_path)
-      expect(user.reload.email).not_to eq(new_email)
+    before do
+      sign_in user
+      visit edit_user_registration_path
     end
 
-    it 'usernameとemailを変更できること' do
-      fill_in "ユーザーネーム", with: "new_username"
-      fill_in "メールアドレス", with: Faker::Internet.email
-      fill_in "現在のパスワード", with: "password"
-      click_button "変更"
-      expect(current_path).to eq(root_path)
-      expect(user.reload.username).to eq(new_username)
-      expect(user.reload.email).to eq(new_email)
+    context "usernameのみ変更する場合" do
+      it "usernameが変更できること" do
+        fill_in "ユーザーネーム", with: "new"
+        click_button "変更"
+        expect(page).to have_current_path(root_path)
+        expect(user.reload.username).to eq("new")
+      end
+    end
+
+    context "username以外も変更する場合" do
+      it "usernameとemailを変更できないこと" do
+        fill_in "ユーザーネーム", with: "new"
+        fill_in "メールアドレス", with: new_email
+        click_button "変更"
+        expect(user.reload.email).not_to eq(user.username)
+        expect(user.reload.email).not_to eq(new_email)
+      end
+
+      it 'usernameとemailを変更できること' do
+        fill_in "ユーザーネーム", with: "new"
+        fill_in "メールアドレス", with: new_email
+        fill_in "現在のパスワード", with: "password"
+        click_button "変更"
+        expect(page).to have_current_path(root_path)
+        expect(user.reload.username).to eq("new")
+        expect(user.reload.email).to eq(new_email)
+      end
     end
   end
 end
