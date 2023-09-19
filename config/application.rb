935f1837e3500aee33c370c530d251b4451e00a7
sasaki <chat1027201@gmail.com>
2023-09-20 04:32:50 +0900

    rubocopの自動修正

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Livehub
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
-    config.i18n.load_path+=Dir[Rails.root.join('config','locales','**','*.yml').to_s]
+    config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.yml").to_s]
 
     # Configuration for the application, engines, and railties goes here.
     #
