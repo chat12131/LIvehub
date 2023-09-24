c8b3e832d986339a3a84470dd2240cc5325529b0
sasaki <chat1027201@gmail.com>
2023-09-25 01:49:21 +0900

    routesでアーティストの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
-  get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'mypage', to: 'users#show'
+
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  get 'mypage', to: 'users#show'
+
+  resources :artists do
+    collection do
+      get :favorites
+    end
+    member do
+      post 'toggle_favorite'
+    end
+  end
+
   root 'home#index'
 end
