f47809ac5ca5428f02431b9894383fd6a1c18d39
sasaki <chat1027201@gmail.com>
2023-09-25 01:42:37 +0900

    artistのmodelとuserの紐付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   mount_uploader :avatar, AvatarUploader
+  has_many :artists, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
