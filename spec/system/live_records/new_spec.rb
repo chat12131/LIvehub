88d7ee09d6d11e1311fbc98fd049eb0657707bb7
sasaki <chat1027201@gmail.com>
2023-10-01 03:33:22 +0900

    liverecord#newの作成とrspec

---
+++ spec/system/live_records/new_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "LiveRecords" do
+  describe "新規ライブ記録作成ページ" do
+    let(:user) { create(:user) }
+
+    before do
+      sign_in user
+      visit new_live_record_path
+    end
+
+    it "ページが正常に表示されること" do
+      expect(page).to have_content("ライブ記録作成")
+    end
+
+    it "正常な情報でライブ記録を作成すると、記録が保存されること", :js do
+      execute_script("document.querySelector('#live_record_date').value = '2023-10-01'")
+      find_by_id('venue-name-display').set("新しい会場名")
+      execute_script("window.scrollBy(0,10000)")
+      click_button "登録"
+
+      expect(page).to have_content("新規作成")
+    end
+  end
+end
