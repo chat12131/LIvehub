3ccc730fc41f1abd9f70748a5299680c3354cbbc
sasaki <chat1027201@gmail.com>
2023-10-01 03:35:26 +0900

    liverecord#showの作成とrspec

---
+++ spec/requests/live_records/show_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "LiveRecords" do
+  let(:user) { create(:user) }
+  let(:live_record) { create(:live_record, user: user) }
+
+  describe "GET /show" do
+    before do
+      sign_in user
+      get live_record_path(live_record)
+    end
+
+    it "正常なレスポンスが返る" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "ライブ記録詳細の基本情報が正しく表示される" do
+      expect(response.body).to include(live_record.name)
+      expect(response.body).to include(live_record.artist.display_name)
+      expect(response.body).to include(live_record.venue.name)
+    end
+
+    it "ライブ記録詳細の日時が正しく表示される" do
+      expect(response.body).to include(live_record.date.to_s)
+    end
+
+    it "メモの情報が正しく表示される" do
+      expect(response.body).to include(live_record.memo)
+    end
+  end
+end
