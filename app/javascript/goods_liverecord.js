99f5393e060abb4993020e259dbea20628f0c8ae
sasaki <chat1027201@gmail.com>
2023-10-03 04:11:19 +0900

    ライブ予定の選択を変更時にメンバーフィールドも変更するように

--- app/javascript/goods_liverecord.js
+++ app/javascript/goods_liverecord.js
@@ -2,6 +2,10 @@ document.addEventListener("turbolinks:load", function() {
   const $liveRecordSelector = $("#good_live_record_id");
   const $dateField = $("#good_date");
   const $artistSelector = $("#good_artist_id");
+  const $addMemberLink = $("#add_member_link");
+  const $memberSelector = $("#good_member_id");
+
+
 
   $liveRecordSelector.on("change", function() {
     const liveRecordId = $(this).val();
@@ -18,6 +22,8 @@ document.addEventListener("turbolinks:load", function() {
           $artistSelector.val(data.artist.id).trigger("change");
         } else {
           $artistSelector.val('');
+          $addMemberLink.css('display', 'none');
+          $memberSelector.html(`<option value="">-アーティストを選択してください-</option>`).prop("disabled", true);
         }
       }).fail(function(jqxhr, textStatus, error) {
         console.error("Request failed:", textStatus, error);
