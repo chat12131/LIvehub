525cc860bbea6da2331a8bd263ba83895fb1703b
sasaki <chat1027201@gmail.com>
2023-10-03 02:48:59 +0900

    statisticsのrequiestとsystemテストの導入

---
+++ spec/system/statistics_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Statistics", type: :system do
+  describe "statistics page" do
+    let(:user) { create(:user) }
+
+    before do
+      login_as(user)
+      visit statistics_path
+    end
+
+    it "displays the correct headings" do
+
+        expect(page).to have_content("統計")
+        expect(page).to have_content("ライブ記録")
+        expect(page).to have_content("支出")
+    end
+  end
+end
