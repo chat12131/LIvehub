d21cf96b5e5f066512c77647b5490976d51a8329
sasaki <chat1027201@gmail.com>
2023-10-03 12:29:55 +0900

    ゲストユーザーの場合アカウント削除できないよう追加

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,21 @@
 
 module Users
   class SessionsController < Devise::SessionsController
+    before_action :ensure_normal_user, only: :destroy
     # before_action :configure_sign_in_params, only: [:create]
+
+    def guest_sign_in
+      user = User.guest
+      sign_in user
+      redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    end
+
+    def ensure_normal_user
+      if resource.email == 'guest@example.com'
+        redirect_to root_path, alert: 'ゲストユーザーは削除できません。'
+        return
+      end
+    end
 
     # GET /resource/sign_in
     # def new
