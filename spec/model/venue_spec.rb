b8bfde158617192a6c5cc325467cf492f4bdfb37
sasaki <chat1027201@gmail.com>
2023-10-09 03:17:55 +0900

    venueモデルへの関連性テスト

---
+++ spec/model/venue_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Venue, type: :model do
+  describe 'associations' do
+    it { should have_many(:live_schedules).dependent(:nullify) }
+    it { should have_many(:live_records).dependent(:nullify) }
+    it { should belong_to(:user) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+end
