b8502c57b423c5c6953e6a3ac12c653f3688f43a
sasaki <chat1027201@gmail.com>
2023-10-09 03:13:04 +0900

    live_scheduleのモデルテストのリファクタリング

--- spec/model/live_record_spec.rb
+++ spec/model/live_record_spec.rb
@@ -2,35 +2,25 @@ require 'rails_helper'
 
 RSpec.describe LiveRecord, type: :model do
   describe 'バリデーション' do
-    let(:live_record) { build(:live_record) }
+    subject(:live_record) { build(:live_record) }
 
-    it '有効な属性値の場合、有効であること' do
-      expect(live_record).to be_valid
-    end
+    it { should validate_presence_of(:date) }
+    it { should validate_numericality_of(:ticket_price).is_greater_than_or_equal_to(0).with_message("はマイナスの値を設定できません。").allow_nil }
+    it { should validate_numericality_of(:drink_price).is_greater_than_or_equal_to(0).with_message("はマイナスの値を設定できません。").allow_nil }
+    it { should validate_length_of(:memo).is_at_most(300).with_message("は300文字以内で入力してください。") }
 
-    it '日付がない場合、無効であること' do
-      live_record.date = nil
-      expect(live_record).not_to be_valid
+    context '未来の日付の場合' do
+      it '無効であること' do
+        live_record.date = Time.zone.tomorrow
+        expect(live_record).not_to be_valid
+      end
     end
+  end
 
-    it '未来の日付の場合、無効であること' do
-      live_record.date = Time.zone.tomorrow
-      expect(live_record).not_to be_valid
-    end
-
-    it 'チケット代がマイナスの場合、無効であること' do
-      live_record.ticket_price = -10
-      expect(live_record).not_to be_valid
-    end
-
-    it 'ドリンク代がマイナスの場合、無効であること' do
-      live_record.drink_price = -5
-      expect(live_record).not_to be_valid
-    end
-
-    it 'メモが301文字以上の場合、無効であること' do
-      live_record.memo = 'あ' * 301
-      expect(live_record).not_to be_valid
-    end
+  describe '関連性' do
+    it { should have_many(:goods).dependent(:nullify) }
+    it { should belong_to(:artist).optional }
+    it { should belong_to(:venue) }
+    it { should belong_to(:user) }
   end
 end
