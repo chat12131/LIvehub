87e9b4a35f2675feb1aaebed95f0de54771eafe7
sasaki <chat1027201@gmail.com>
2023-10-09 05:53:00 +0900

    ライブ記録が初期状態で選択されている場合にも対応、ライブ記録をからにしたらアーティストも空に変更

--- app/javascript/goods_liverecord.js
+++ app/javascript/goods_liverecord.js
@@ -5,11 +5,7 @@ document.addEventListener("turbolinks:load", function() {
   const $addMemberLink = $("#add_member_link");
   const $memberSelector = $("#good_member_id");
 
-
-
-  $liveRecordSelector.on("change", function() {
-    const liveRecordId = $(this).val();
-
+  function fetchLiveRecordDetails(liveRecordId) {
     if (liveRecordId) {
       $.getJSON(`/live_records/${liveRecordId}/details.json`, function(data) {
         if (data.date) {
@@ -21,12 +17,25 @@ document.addEventListener("turbolinks:load", function() {
         if (data.artist && data.artist.id) {
           $artistSelector.val(data.artist.id).trigger("change");
         } else {
-          $artistSelector.val('');
+          $artistSelector.val('').trigger("change"); // ここを追加
           $addMemberLink.css('display', 'none');
           $memberSelector.html(`<option value="">-アーティストを選択してください-</option>`).prop("disabled", true);
         }
       }).fail(function(jqxhr, textStatus, error) {
       });
+    } else {
+      $dateField.val('');
+      $artistSelector.val('').trigger("change"); // ライブ記録が選択されていない場合の処理
+      $addMemberLink.css('display', 'none');
+      $memberSelector.html(`<option value="">-アーティストを選択してください-</option>`).prop("disabled", true);
     }
+  }
+
+  $liveRecordSelector.on("change", function() {
+    const liveRecordId = $(this).val();
+    fetchLiveRecordDetails(liveRecordId);
   });
+
+  const initialLiveRecordId = $liveRecordSelector.val();
+  fetchLiveRecordDetails(initialLiveRecordId);
 });
