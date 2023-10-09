638b877f9bf349a8b3015c1de3027a575460b88a
sasaki <chat1027201@gmail.com>
2023-10-09 21:55:43 +0900

    メンバー削除のアクションを設定、ajax

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,4 +3,14 @@ class MembersController < ApplicationController
     @members = Member.where(artist_id: params[:artist_id])
     render json: @members
   end
+
+  def destroy
+    @member = Member.find(params[:id])
+    @artist = @member.artist
+    @member.destroy
+    respond_to do |format|
+      format.html { redirect_to artist_path(@artist) }
+      format.js { render 'artists/destroy' }
+    end
+  end
 end
