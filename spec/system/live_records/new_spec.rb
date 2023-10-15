be4c9b5e611447763f821e2cf3f5f02f4e3eb3b8
sasaki <chat1027201@gmail.com>
2023-10-15 21:04:02 +0900

    ライブ記録作成ページで登録されないことと、アーティスト追加ボタンをテスト

--- spec/system/live_records/new_spec.rb
+++ spec/system/live_records/new_spec.rb
@@ -9,17 +9,26 @@ RSpec.describe "LiveRecords" do
       visit new_live_record_path
     end
 
-    it "ページが正常に表示されること" do
-      expect(page).to have_content("ライブ記録")
-    end
-
     it "正常な情報でライブ記録を作成すると、記録が保存されること", :js do
       execute_script("document.querySelector('#live_record_date').value = '2023-10-01'")
       find_by_id('venue-name-display').set("新しい会場名")
       execute_script("window.scrollBy(0,10000)")
       click_button "登録"
 
-      expect(page).to have_content("新規作成")
+      expect(page).to have_current_path live_records_path
+    end
+
+    it "不正な情報でライブ記録が作成されないこと", :js do
+      execute_script("document.querySelector('#live_record_date').value = '2023-10-01'")
+      execute_script("window.scrollBy(0,10000)")
+      click_button "登録"
+
+      expect(page).to have_content("保存されませんでした")
+    end
+
+    it "新しいアーティストを追加のリンクをクリックすると新しいアーティストのページへ遷移すること", :js do
+      click_link "新しいアーティストを追加"
+      expect(page).to have_current_path("/artists/new", ignore_query: true)
     end
   end
 end
