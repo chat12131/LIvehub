b052d90cc003f7bafd34da12586f75240b83b71a
sasaki <chat1027201@gmail.com>
2023-10-15 20:56:18 +0900

    ログインページのrspecを追加

---
+++ spec/system/users/user_new_sessions_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "UserRegistration" do
+  let(:user) { create(:user) }
+  before do
+    visit new_user_session_path
+  end
+
+  it "usernameが正しく登録されること" do
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_button "GO"
+    expect(page).to have_current_path(root_path)
+  end
+
+  it "usernameが登録されないこと" do
+    fill_in "Email", with: "username"
+    fill_in "Password", with: "test@example.com"
+
+    click_button "GO"
+    expect(page).to have_current_path(new_user_session_path)
+  end
+end
