28ef781b31a0ce7eb06c6f753a635fcb102196a4
sasaki <chat1027201@gmail.com>
2023-10-15 20:54:28 +0900

    ライブ予定のファクトリーボットを修正

--- spec/factories/live_schdual.rb
+++ spec/factories/live_schdual.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :live_schedule do
     name { "Sample Live Event" }
     artist { FactoryBot.create(:artist) }
-    date { Time.zone.today + 30.days }
-    open_time { 6.hours.from_now }
-    start_time { 7.hours.from_now }
+    date { Time.zone.today + 3.days }
+    open_time { 1.hours.from_now }
+    start_time { 2.hours.from_now }
     venue { FactoryBot.create(:venue) }
-    ticket_status { 0 }
+    ticket_status { 1 }
     ticket_price { 3000 }
     drink_price { 500 }
     memo { "Some additional notes about the event." }
